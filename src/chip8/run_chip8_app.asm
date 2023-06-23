cf5b5795a1ab3d5f85e9f06ac3b01cf0ee577ae9
Nihal Kenkre <nihal.kenkre@gmail.com>
2023-06-23 13:45:27 +0530

    [Chip 8]: convert the little endian stored chip8 opcode data to big endian and process
    [Chip8]: WIP opcode fetch

--- src/chip8/run_chip8_app.asm
+++ src/chip8/run_chip8_app.asm
@@ -254,6 +254,8 @@ fill_reserve_bytes:
 
 ;
 ; Fetches the next 2 byte opcode from the 'chip8' memory and executes it
+; Chip8 is Big Endian and x86 is Little Endian
+; So the higher bytes of the opcode are stored as the lower bytes in the memory addr
 ;
 ; Params:
 ;   si: ptr to memory
@@ -264,22 +266,44 @@ execute_next_opcode:
     mov bp, sp
 
     push ax
+    push bx
+    push cx
+    push dx
 
-    lodsw               ; opcode is in ax
+    mov bx, [chip8_pc]
+    mov ax, [bx]                        ; opcode is in ax
+    ror ax, 8                           ; converting the 2 byte x86 Little Endian data to Chip8 Big Endian data
 
-    push ax
-    shr ax, 12          ; shift right by 12 to get the 'first' nibble
+    ;; Check the most significant nibble of the opcode to determine
+    ;; the functionality required
+    push ax                             ; this value will be needed to check the complete opcode
+    and ax, 0xf000
+    shr ah, 4                           ; this will leave the most significant nibble in ah
 
-    cmp al, 0
+    cmp ah, 0
     je .first_0
 
-    cmp al, 1
+    cmp ah, 1
     je .first_1
+
+    cmp ah, 2
+    je .first_2
+
+    cmp ah, 6
+    je .first_6
+
+    cmp ah, 0xa
+    je .first_a
+
+    cmp ah, 0xd
+    je .first_d
+
+    add word [chip8_pc], 2
 
     jmp .return
 
 .first_0:
-    pop ax
+    pop ax                              ; retrieve the full opcode
     
     cmp al, 0xe0
     je ._00e0
@@ -287,14 +311,100 @@ execute_next_opcode:
     cmp al, 0xee
     je ._00ee
 
+    jmp .return
+
 ._00e0:
     call clear_screen_graphics
-    add byte [chip8_pc], 2
+    add word [chip8_pc], 2
+
+    jmp .return
 
 ._00ee:
+    mov bx, [chip8_sp]                  ; sp is in bx
+    mov ax, [chip8_stack + bx]
+    mov [chip8_pc], ax
 
+    dec byte [chip8_sp]
+
+    jmp .return
+
+.first_1:
+    pop ax                              ; retrieve the full opcode
+
+    and ax, 0x0fff
+    mov [chip8_pc], ax
+
+    jmp .return
+
+.first_2:
+    pop ax
+
+    and ax, 0x0fff                      ; filter out the new pc
+
+    mov cx, [chip8_pc]                  ; mov pc value to cx
+    add cx, 2                           ; add 2 to the pc
+    mov bx, [chip8_sp]                  ; mov sp value to bx
+    mov [chip8_stack + bx], cx          ; mov the pc value to the stack
+
+    inc byte [chip8_sp]                 ; increment sp since we pushed somethig on the stack
+
+    mov [chip8_pc], ax                  ; mov new pc to the pc
+    
+    jmp .return
+
+.first_6:
+    pop ax                              ; retrieve the full opcode
+
+    and ah, 0x0f
+    mov bl, ah
+
+    mov [chip8_V + bx], al
+    add word [chip8_pc], 2
+
+    jmp .return
+
+.first_a:
+    pop ax                              ; retrieve the full opcode
+
+    and ax, 0x0fff
+    mov [chip8_I], ax
+
+    add word [chip8_pc], 2
+
+    jmp .return
+
+.first_d:
+    pop ax                              ; retrieve the full opcode
+    
+    and ax, 0x0fff                      ; *XYN
+
+    push ax                             ; save xyn so we can extract x y n
+    and al, 0x0f
+    xor cx, cx
+    mov cl, al                          ; cl contains n
+    pop ax
+
+    push ax
+    and al, 0xf0
+    shr al, 4
+    xor bx, bx
+    mov bl, al                          ; bl contains y
+    pop ax
+
+    push ax                             ; save xyn so we can extract x y n
+    and al, 0xf0
+    xor dx, dx
+    mov dl, al                          ; dl contains x
+    pop ax
+
+    add word [chip8_pc], 2
+
+    jmp .return
 
 .return:
+    pop dx
+    pop cx
+    pop bx
     pop ax
 
     mov sp, bp
@@ -321,15 +431,13 @@ run_chip8_app:
     mov [chip8_pc], di
     call load_sectors
 
-    mov ax, 0x0013                        ; 320x200 graphics mode 
+    mov ax, 0x0013                          ; 320x200 graphics mode 
     int 0x10
 
     mov ax, 0xa000
     mov es, ax
 
     mov di, 320 * 100 + 160
-
-    mov si, [chip8_memory]
 
 .timer_loop:                                ; TIMER HACK !!!
     mov ecx, 0xffffff
@@ -375,6 +483,9 @@ sector_size equ 512
 chip8_memory: dw 0
 chip8_pc: dw 0
 chip8_stack: times 16 dw 0
-chip8_sp: dw 0
+chip8_sp: db 0                      ; This can be an offset addr into the 'chip8_stack' memory
+chip8_V: times 16 db 0
+chip8_I: db 0
+chip8_display_buffer: times 64 * 32 db 0
 
 %endif
