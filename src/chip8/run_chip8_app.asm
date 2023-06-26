bb0f8da5baf3f755dfcb2fd729271d2f1ade3ab8
Nihal Kenkre <nihal.kenkre@gmail.com>
2023-06-26 14:34:52 +0530

    [Chip 8]: WIP drawing to screen

--- src/chip8/run_chip8_app.asm
+++ src/chip8/run_chip8_app.asm
@@ -34,9 +34,10 @@ fill_reserve_bytes:
     push bp
     mov bp, sp
 
+    mov di, chip8_memory
+
     push ax
     push cx
-    push di                 ; save initial di for later padding calculations
 
     ; 0
     mov al, 0xf0
@@ -230,20 +231,6 @@ fill_reserve_bytes:
     mov al, 0x80
     stosb
 
-    ; increment di to the 512 boundary
-    ; first find the difference of the current di from initial di
-
-    pop cx                  ; initial di is in cx
-    mov ax, di              ; current di is in ax
-    sub ax, cx              ; ax = cx - ax
-
-    ; subtract the remainder from the sector size and add the diff
-    ; to the current di
-    mov cx, sector_size
-    sub cx, ax              ; cx = ax -cx
-
-    add di, cx              ; di at the 512 boundary
-
     pop cx
     pop ax
 
@@ -384,7 +371,7 @@ execute_next_opcode:
     mov cl, al                          ; cl contains n
     pop ax
 
-    push ax
+    push ax                             ; save xyn so we can extract x y n
     and al, 0xf0
     shr al, 4
     xor bx, bx
@@ -392,10 +379,72 @@ execute_next_opcode:
     pop ax
 
     push ax                             ; save xyn so we can extract x y n
-    and al, 0xf0
+    and ah, 0x0f
     xor dx, dx
-    mov dl, al                          ; dl contains x
+    mov dl, ah                          ; dl contains x
     pop ax
+
+    ; Get the y co-ordinate, and store it in bl
+    mov bl, [chip8_V + bx]
+    and bl, 31
+
+    mov byte [chip8_V + 0xa], 0
+
+    mov ch, 0                           ; this will be the index of the counter, going to cl
+
+    .n_loop:
+        ; Get the Nth row byte into ax
+        ; row = chip8_memory[chip8_I + n]
+        
+        ; First calculate the chip_I + n
+        mov ax, [chip8_I]
+
+        push cx
+        xchg ch, cl 
+        xor ch, ch
+        add ax, cx                          ; ax containx chip8_I + n
+
+        pop cx
+
+        ; mov ax to bx 
+        ; since bx can be used as an offset
+        push bx
+        mov bx, ax
+        xor ax, ax
+        mov al, [chip8_memory + bx]        ; al contains the Nth row byte
+        ; ror al, 4                          ; x86 little endian to chip8 big endian
+        pop bx
+        
+        push cx                            ; cx will be used for row bittest
+
+        mov cx, 7                          ; bit index
+
+        ; al contains the Nth row byte, chip8_display_buffer contains the bytes 
+        ;    currently in the display buffer
+        ; XOR the individual bits of al with the bits starting at chip8_display_buffer[x][y]
+        ;    and store the result in chip8_display_buffer[x][y]
+        .row_loop:
+        .row_byte_compare:                         ; check if row byte bit at cx is set
+            bt ax, cx
+        .display_compare:
+            push dx
+            mov dl, [chip8_display_buffer + dx]
+            mul dl,
+            bt dx, cx
+            pop dx
+
+            jz .row_loop_end
+
+            dec cx
+            jmp .row_loop
+
+        .row_loop_end:
+
+        pop cx
+
+        inc byte ch
+        cmp ch, cl
+        jne .n_loop
 
     add word [chip8_pc], 2
 
@@ -413,22 +462,21 @@ execute_next_opcode:
     ret
 
 ;
-; Runs the Chip8 program located in memory
+; Loads the Chip8 program into memory and runs it.
 ; Params:
 ;   bx: start sector
 ;   cx: sector count
 ;
-;   di: addr where the interpreter starts, first 512 reserved. font data is here
-;       app data can be stored at di + 512
-;   
 [bits 16]
 run_chip8_app:
     push bp
     mov bp, sp
 
-    mov [chip8_memory], di
     call fill_reserve_bytes
-    mov [chip8_pc], di
+
+    mov word [chip8_pc], chip8_memory + 0x200
+    mov di, [chip8_pc]
+
     call load_sectors
 
     mov ax, 0x0013                          ; 320x200 graphics mode 
@@ -480,12 +528,14 @@ esc_scan_code equ 0x01
 
 sector_size equ 512
 
-chip8_memory: dw 0
+chip8_memory: times 4096 db 0               ; IMP: allocatin 4kb memory. Please adjust this to accomodate the maximum size of the available ROMs
+                                            ; addr where the interpreter starts, first 512 reserved. font data is here
+                                            ; app data can be stored at chip8_memory + 512
 chip8_pc: dw 0
 chip8_stack: times 16 dw 0
-chip8_sp: db 0                      ; This can be an offset addr into the 'chip8_stack' memory
+chip8_sp: db 0                              ; This is an offset addr into the 'chip8_stack' memory
 chip8_V: times 16 db 0
-chip8_I: db 0
-chip8_display_buffer: times 64 * 32 db 0
+chip8_I: dw 0
+chip8_display_buffer: times 32 db 0         ; each bit of the 32 bytes is a pixel, which can be set/unset. each hor. line has 8 bytes, and ver. line has 4 bytes
 
 %endif
